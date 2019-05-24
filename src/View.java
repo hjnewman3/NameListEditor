1e56f5fd4b25d7f895c46cff83eccbcb8c8f421c
Chip Newman <chip594@gmail.com>
2019-05-24 14:15:39 -0700

    create File Menu, no action

--- src/View.java
+++ src/View.java
@@ -1,9 +1,39 @@
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.MenuBar;
+
 public class View {
 
     private final Controller controller;
+    private final BorderPane layout;
 
-    public View(Model model, Controller, controller) {
+    public View(Model model, Controller controller) {
 
         this.controller = controller;
+
+        layout = new BorderPane();
+
+        VBox topMenu = new VBox();
+
+        MenuBar menuBar = new MenuBar();
+
+        // File Menu
+        Menu fileMenu = new Menu("File");
+        MenuItem addToFile = new MenuItem("Add to Current List");
+        // TODO: add action
+
+        MenuItem saveOption = new MenuItem("Save");
+        // TODO: add action
+
+        MenuItem closeOption = new MenuItem("Close");
+        // TODO: add action
+
+        MenuItem openOption = new MenuItem("Open");
+        // TODO: add action
+
+        fileMenu.getItems().addAll(openOption, addToFile, saveOption, closeOption);
+
     }
 }
